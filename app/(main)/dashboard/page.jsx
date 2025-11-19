abed74449ca21cd451dbdaf9c295ee4eba599af9
SudarshanAhire <sudarshanahire292@gmail.com>
2025-11-19 11:46:32 +0530

    commit1

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { getUserOnboardingStatus } from '@/actions/user'
+import { redirect } from 'next/navigation'
+import { get } from 'react-hook-form';
+import { getIndustryInsights } from '@/actions/dashboard';
+import DashboardView from './_components/dashboard-view';
+
+const IndustryInsightsPage = async() => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (!isOnboarded) {
+    redirect('/onboarding');
+  }
+
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights}/>
+    </div>
+  )
+}
+
+export default IndustryInsightsPage
